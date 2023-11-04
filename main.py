d33e879196673aa75751868a73d10efb9efc9a28
Tanzeel Jaffery <trjaffery04@gmail.com>
2023-11-04 17:26:39 -0500

    new project

---
+++ main.py
@@ -0,0 +1,21 @@
+import html
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for question in question_data:
+    question_text = html.unescape(question["question"])
+    question_answer = html.unescape(question["correct_answer"])
+    new_q = Question(question_text, question_answer)
+    question_bank.append(new_q)
+
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz!")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
